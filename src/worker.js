b3186463daf07bf740d78f898b0afca8aa32c37c
Josh Dickie <josh.dickie@live.com.au>
2026-02-22 09:24:55 +1100

    test deploy

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request, env, ctx) {
-    return new Response("warband worker alive", {
+    return new Response("warband worker alive, v2 test deploy", {
       headers: { "content-type": "text/plain" }
     });
   }
