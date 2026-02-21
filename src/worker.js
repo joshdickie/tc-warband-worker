068fef6698ad3ae20332ea06467f7338852e26f9
Josh Dickie <josh.dickie@live.com.au>
2026-02-22 09:42:40 +1100

    another deploy test

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,6 @@
 export default {
   async fetch(request, env, ctx) {
-    return new Response("warband worker alive, v2 test deploy", {
+    return new Response("hello emma world can i haz cheezburger", {
       headers: { "content-type": "text/plain" }
     });
   }
