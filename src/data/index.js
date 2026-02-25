2e4f3b1ad187c63b7b38d82aca66104b8b524060
Josh Dickie <josh.dickie@live.com.au>
2026-02-25 13:26:02 +1100

    got started on normalising db

--- src/data/index.js
+++ src/data/index.js
@@ -1,38 +1,41 @@
-import abilities from "./abilities.json";
-import armour from "./armour.json";
-import equipment from "./equipment.json";
-import factions from "./factions.json";
-import injuries from "./injuries.json";
-import models from "./models.json";
-import skills from "./skills.json";
-import weaponsMelee from "./weapons_melee.json";
-import weaponsRanged from "./weapons_ranged.json";
+import data from "./db.json"
 
-function getOrThrow(table, id, type) {
-  const result = table[id];
-  if (!result) throw new Error(`Missing ${type} id=${id}`);
-  return result;
+function getOrThrow(table, path) {
+  if (typeof path !== "string" || path.length === 0) {
+    throw new Error(`Can not pull data with empty path`);
+  }
+
+  const parts = path.replace(/^./, "").split(".");
+
+  let current = table;
+
+  for (const part of parts) {
+    if (
+      current === null ||
+      current === undefined ||
+      typeof current !== "object" ||
+      !(part in current)
+    ) {
+      throw new Error(`Missing data: "${path}" failed at "${part}"`);
+    }
+
+    current = current[part];
+  }
+
+  return current;
 }
 
 export function createDb() {
   return {
-    abilities,
-    armour,
-    equipment,
-    factions,
-    injuries,
-    models,
-    skills,
-    weaponsMelee,
-    weaponsRanged,
-    getAbility: (id) => getOrThrow(abilities, id, "ability"),
-    getArmour: (id) => getOrThrow(armour, id, "armour"),
-    getEquipment: (id) => getOrThrow(equipment, id, "equipment"),
-    getFaction: (id) => getOrThrow(factions, id, "faction"),
-    getInjury: (id) => getOrThrow(injuries, id, "injury"),
-    getModel: (id) => getOrThrow(models, id, "model"),
-    getSkill: (id) => getOrThrow(skills, id, "skill"),
-    getWeaponMelee: (id) => getOrThrow(weaponsMelee, id, "weaponMelee"),
-    getWeaponRanged: (id) => getOrThrow(weaponsRanged, id, "weaponRanged"),
+    data,
+    // getAbility: (id) => getOrThrow(abilities, id, "ability"),
+    // getArmour: (id) => getOrThrow(armour, id, "armour"),
+    // getEquipment: (id) => getOrThrow(equipment, id, "equipment"),
+    // getFaction: (id) => getOrThrow(factions, id, "faction"),
+    // getInjury: (id) => getOrThrow(injuries, id, "injury"),
+    // getModel: (id) => getOrThrow(models, id, "model"),
+    // getSkill: (id) => getOrThrow(skills, id, "skill"),
+    // getWeaponMelee: (id) => getOrThrow(weaponsMelee, id, "weaponMelee"),
+    // getWeaponRanged: (id) => getOrThrow(weaponsRanged, id, "weaponRanged"),
   };
 }
